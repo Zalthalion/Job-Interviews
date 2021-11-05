e8e9b948d3956d3993d2284abbc48db231a8c983
Zalthalion <freidenfeldsjuris@gmail.com>
2021-11-06 00:16:04 +0200

    First initial push
    Contains two projects - one for functionality and one for unit tests

---
+++ PTEncontrolBaltic/PTEncontrolBaltic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PTEncontrolBaltic.RomanNumerals;
+
+namespace PTEncontrolBaltic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            //Possible place for user input
+            //Was not specified - therefore wont create this block
+            //
+        }
+    }
+}
